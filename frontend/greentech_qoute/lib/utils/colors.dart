6ad32c34f1492a3e572dcbdc46e6e98f4c352609
Dean Edward D. Magalong <deanmagalong@gmail.com>
2026-03-11 09:13:11 +0800

    refactor: start fresh from main.dart

--- frontend/greentech_qoute/lib/utils/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  AppColors._(); // Prevent instantiation
-
-  static const Color fernGreen = Color(0xFF4F7942);
-  static const Color fernGreenLight = Color(0xFF6A9A5C);
-  static const Color emeraldGreen = Color(0xFF059669); // PDF / AppBar color
-}
